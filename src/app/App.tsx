00e3c1ae5a2dd836916cd205e9805bc199c3772d
rnfltpgus <jpp901220@gmail.com>
2022-11-30 23:41:01 +0900

    🔩 Feat: 초기 환경 셋팅 CLI

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>안녕하세요. 정세현입니다.</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
